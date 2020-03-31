ab81492e6c39a79b9cb068ab92f685dedc2bacbc
Angom-Naomi <angomnd@gmail.com>
2020-03-30 18:22:53 -0700

    feature(homepage and signup):
    apply javascript to make signup buttons interactive
    
    write a function to capture user input
    user input must conform to the standard regex format
    apply javascript to make the page interactive
    signup buttons should respond to events
    modify index.html
    apply css styles
    
    [Finishes #172018175, #172017128]

---
+++ javascript/project.js
@@ -0,0 +1,34 @@
+// Code for 'create account' button
+function createAccount(){
+
+    // function approval(){ 
+        var username = document.getElementById("name").value; //capture the user name value a user enters
+        var useremail = document.getElementById("email").value; //capture the email value a user enters
+        var pw = document.getElementById("password").value; //capture the password value a user enters
+
+        var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; //regex format for emails
+        var pwFormat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/; //regex for a strong password
+   
+        if (username === " " || useremail === "") {
+            alert("Please fill out all fields...");
+            return false;
+        } 
+        else if (!(useremail.match(emailFormat))){
+            alert("Your Email is invalid...");
+            return false;
+        }
+        else if(!(pw.match(pwFormat))){
+            alert("Password must have 8 characters, atleast 1 lowercase and uppercase letter, and 1 special character")
+            return false;
+        }
+
+        else {
+            document.getElementById("acc").submit(); 
+            alert("Your account has been created successfully......");
+            window.location.href="menu.html";
+            
+        }
+
+};
+
+
