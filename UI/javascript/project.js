55da481a9f20b1db96c7ec5e7a683910ad4d9af5
Angom-Naomi <angomnd@gmail.com>
2020-04-01 08:54:33 -0700

    feature(signup section): fix the 3 signup buttons
    signup buttons should redirect to menu.html page after successful account creation
    
    [Fixes #172018175, #172017128]

--- UI/javascript/project.js
+++ UI/javascript/project.js
@@ -4,10 +4,8 @@ function createAccount(){
         var useremail = document.getElementById("email").value; //capture the email value a user enters
         var pw = document.getElementById("password").value; //capture the password value a user enters
         var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; //regex format for emails
-        var pwFormat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/; //regex for a strong password
+        var pwFormat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?!.*\s).{8,}$/; //regex for a strong password
    
-
-        // function approval(){ 
         if (username === " " || useremail === "") {
             alert("Please fill out all fields...");
             return false;
@@ -23,12 +21,75 @@ function createAccount(){
 
         else {
             alert("Your account has been created successfully......");
-            document.getElementById("acc").submit(); 
             window.location.href="../menu.html";
             return true;
             
         }
-
 };
+
+
+//Sign up with google button
+function googleSignup(){
+    var username = document.getElementById("name").value; //capture the user name value a user enters
+    var useremail = document.getElementById("email").value; //capture the email value a user enters
+    var pw = document.getElementById("password").value; //capture the password value a user enters
+    var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; //regex format for emails
+    var pwFormat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?!.*\s).{8,}$/; //regex for a strong password
+
+    if (username === " " || useremail === "") {
+        alert("Please fill out all fields...");
+        return false;
+    } 
+    else if (!(useremail.match(emailFormat))){
+        alert("Your Email is invalid...");
+        return false;
+    }
+    else if(!(pw.match(pwFormat))){
+        alert("Password must have 8 characters, atleast 1 lowercase and uppercase letter, and 1 special character")
+        return false;
+    }
+
+    else {
+        alert("Your account has been created successfully......");
+        window.location.href="../menu.html";
+        return true;
+        
+    }
+};
+
+//Sign up with Facebook button
+function fbSignup(){
+    var username = document.getElementById("name").value; //capture the user name value a user enters
+    var useremail = document.getElementById("email").value; //capture the email value a user enters
+    var pw = document.getElementById("password").value; //capture the password value a user enters
+    var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; //regex format for emails
+    var pwFormat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?!.*\s).{8,}$/; //regex for a strong password
+
+    if (username === " " || useremail === "") {
+        alert("Please fill out all fields...");
+        return false;
+    } 
+    else if (!(useremail.match(emailFormat))){
+        alert("Your Email is invalid...");
+        return false;
+    }
+    else if(!(pw.match(pwFormat))){
+        alert("Password must have 8 characters, atleast 1 lowercase and uppercase letter, and 1 special character")
+        return false;
+    }
+
+    else {
+        alert("Your account has been created successfully......");
+        window.location.href="../menu.html";
+        return true;
+        
+    }
+};
+
+
+
+
+
+
 
 
