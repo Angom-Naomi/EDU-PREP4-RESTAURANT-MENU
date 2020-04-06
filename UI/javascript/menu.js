88e9866ddefaea9ddb67cabb13a973e54f7be756
Angom-Naomi <angomnd@gmail.com>
2020-04-06 05:39:23 -0700

    feature(menu):
    Modify the entire menu
    
    delete gallery page
    add images to the menu
    apply JavaScript to enable makeOrder
    fix the make order section to give total bill
    add social media images in footer as links
    add a search tab
    link all pages
    
    [Finishes #171996894, #171996998, #171997013, #171997033, #172017427, #172017483, #172119981, #172017964]

--- UI/javascript/menu.js
+++ UI/javascript/menu.js
@@ -1,160 +1,110 @@
-                //SEARCH BAR
-function search(){
-    var item = document.getElementById("searchBox").value;
-    if (item===" "){
-        alert("Enter your search...")
-        return false;
-    }
-    else {
-        alert("Your" + " " + item + " " + "is here...")
-    }
-};
 
-
-        //MAKE ORDER
-//First load the DOM document 
+            // FOOD MENU
+//First load the document/DOM 
 if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', ready);
 } 
 else {
     ready();
-}
+};
 
-//Activate buttons when the dom content load is ready
+//When the document loads
 function ready() {
-
-    var removeButtons = document.getElementsByClassName('remove-btn');
-    for(var r = 0; r < removeButtons.length; r++) {
-        var button = removeButtons[r];
-        button.addEventListener('click', removeFromMenu); 
+    var removeButtons = document.getElementsByClassName('btn-danger');
+    for (var i = 0; i < removeButtons.length; i++) {
+        var button = removeButtons[i];
+        button.addEventListener('click', removeItem );
     }
-​
-    var qnty = document.getElementsByClassName('order-qnty');
-    for(var q = 0; q < qnty.length; q++) {
-        var input = qnty[q];
-        input.addEventListener('change', changeQnty);
-​   }
 
-    var addButtons = document.getElementsByClassName('addBtn');
-    for(var x = 0; x < addButtons.length; x++) {
-        var button = addButtons[x];
-        button.addEventListener('click', addToMenu);
-         }
-
-    document.getElementsByClassName('orderBtn')[0].addEventListener('click', makeOrder);
-};
-
-// The functions
-​
-// the purchase button
-function makeOrder(event) {
-    alert('Gracias! Come again next time');
-    var item = document.getElementsByClassName('order-item')[0]
-    // continue to execute while what is inside () is true
-    while (item.hasChildNodes()) {
-        item.removeChild(item.firstChild);
+    var quantityInputs = document.getElementsByClassName('order-quantity-input');
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i];
+        input.addEventListener('change', quantityChanged );
     }
-    myTotal()
-};
-​
-// quantity input changes in menu
-function changeQnty(event) {
-   var input = event.target;
-    if(isNaN(input.value) ||  input.value <= 0){        // check if input value is not a number and is not less than or equal to zero
-        input.value = 1;              // set the lowest input value
-    }    
-    myTotal()
-};
-​
-// remove items from menu
-function removeFromMenu(event) { 
-​    var btn = event.target;
-    // the button is inside two parent elements
-    btn.parentElement.parentElement.remove();
-    myTotal()
-};
-​
-// add items to menu
-function addToMenu(event) {
-    var button = event.target;
-    var addItem = button.parentElement.parentElement;
-    // get the text inside the element
-    var title = addItem.getElementsByClassName('item')[0].innerText;
-    var price = addItem.getElementsByClassName('price')[0].innerText;
-    // htis is the add item cart METHOD
-    addToMenu(title, price);
-    myTotal()
+
+    var addButtons = document.getElementsByClassName('shop-item-button');
+    for (var i = 0; i < addButtons.length; i++) {
+        var button = addButtons[i];
+        button.addEventListener('click', addItem );
+    }
+
+    document.getElementsByClassName('btn-makeOrder')[0].addEventListener('click', makeOrder );
 };
 
-// add items to menu
-function addItemToCart(item, price) {
-    // create a row for our menu items / new div
-    var orderRow = document.createElement('div');
-    orderRow.classList.add('order-row');
-    // add a row to cmenu items
-    var orderItems = document.getElementsByClassName('order-item')[0];
-​
-    // this is code to check if an items has already been added to menu
-    var orderItemNames = orderItems.getElementsByClassName('order-item-title');
-    for(var i = 0; i < orderItemNames.length; i++) {
-        if (orderItemNames[i].innerText == title) {
-            alert('This item already exists on your menu!');
-            // exit function and stop executing code below it, bringing you back to start if add item function
-            return 
-        }
-        
+                //FUNCTIONS AT WORK
+function makeOrder() {
+    alert('Gracias! Come again soon and thank you.');
+    var orderItems = document.getElementsByClassName('order-items')[0];
+    while (orderItems.hasChildNodes()) {
+        orderItems.removeChild(orderItems.firstChild);
     }
-​
-    // add order row to the very end of the order items / use copied html (order-row contents) to generate an order row / using back ticks (``) so we can use our string on different lines
-    var  orderRowContents = `
-        <div class="order-row">
-                <span class="order-item order-header order-column">${title}</span>
-                <span class="order-price order-header order-column">${price}</span>
-                <span class="order-qnty order-header order-column">Quantity</span>
-                <button type="button" class="btn removeBtn" onclick="removeFromMenu(event)">remove</button>         
-        </div>`
-    // ${ } is used to store varibales directly into our code 
-    // anything insde of it will be a variable that will evaluate
-    // we use innerhtml because we are using html tags inside here
-    orderRow.innerHTML = orderRowContents;
-    orderItems.append(orderRow); 
-    // can remove added items
-    orderRow.getElementsByClassName('remove-btn')[0].addEventListener('click', removeFromMenu);
-    // total will update with each added item
-    orderRow.getElementsByClassName('order-quantity-input')[0].addEventListener('change', changeQnty)
-​
+    updateTotal();
 }
-​
-​
-// update order total function
-function theTotal() {
-    // select first element in array as our cart item container
-    var orderItemContainer = document.getElementsByClassName('order-item')[0];
-    // only gets element inside of the object with the specified class;
-    var cartRows = orderItemContainer.getElementsByClassName('order-row')
+
+function removeItem(event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+    updateTotal();
+};
+
+function quantityChanged(event) {
+    var input = event.target;
+    if (isNaN(input.value) || input.value <= 0) {  //quantity should not be 0 or negative
+        input.value = 1;
+    }
+    updateTotal();
+};
+
+function addItem(event) {
+    var button = event.target;
+    var shopItem = button.parentElement.parentElement;
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText;
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText;
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src;
+    addItemToOrder(title, price, imageSrc);  //new function to add items to make order
+    updateTotal();
+};
+
+function addItemToOrder(title, price, imageSrc) {
+    var orderRow = document.createElement('div');  //create a new div for make order
+    orderRow.classList.add('order-row');
+    var orderItems = document.getElementsByClassName('order-items')[0];
+    var orderItemNames = orderItems.getElementsByClassName('order-item-title');
+    for (var i = 0; i < orderItemNames.length; i++) {
+        if (orderItemNames[i].innerText == title) {
+            alert('This item is already added to the cart');  //if item appears more than once
+            return;
+        }
+    }
+    //variable to hold the whole new HTML div
+    var orderRowContents = `
+        <div class="order-item order-column">
+            <img class="order-item-image" src="${imageSrc}" width="100px" height="100px">
+            <span class="order-item-title">${title}</span>
+        </div>
+        <span class="order-price order-column">${price}</span>
+        <div class="order-quantity order-column">
+            <input class="order-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">Remove</button>
+        </div>`
+    orderRow.innerHTML = orderRowContents;
+    orderItems.append(orderRow);
+    orderRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeItem);
+    orderRow.getElementsByClassName('order-quantity-input')[0].addEventListener('change', quantityChanged);
+};
+
+//Clear/refresh table after user makes order
+function updateTotal() {
+    var orderItemContainer = document.getElementsByClassName('order-items')[0];
+    var orderRows = orderItemContainer.getElementsByClassName('order-row');
     var total = 0;
-    
-    for (var i =0; i < orderRows.length; i++) {
+    for (var i = 0; i < orderRows.length; i++) {
         var orderRow = orderRows[i];
-        // get row price
-        var px = orderRow.getElementsByClassName('order-price')[0];
-        // get row quantity
-        var qunty = orderRow.getElementsByClassName('order-quantity-input')[0];
-        // get the price from price element, change string to number value with decimals, remove UGX from our string
-        var price = parseFloat(px.innerText.replace('Ugshs', ''));
-        // get quantity from our quantity element
-        var quantity = qunty.value;
-        // calculating total price, will do each iteration
+        var priceElement = orderRow.getElementsByClassName('order-price')[0];
+        var quantityElement = orderRow.getElementsByClassName('order-quantity-input')[0];
+        var price = parseInt(priceElement.innerText.replace('Ugx', ''));
+        var quantity = quantityElement.value;
         total = total + (price * quantity);
     }
-    // round off total to two decimal places
-    total = Math.round(total * 100) / 100;
-    // get order-total-price element, set inner text of element to total
-    document.getElementsByClassName('order-total-price')[0].innerText = 'Ugshs' + total;
-}
-
-
-
-
-
-
+    document.getElementsByClassName('order-total-price')[0].innerText = total + " " + "Ugx";
+};
